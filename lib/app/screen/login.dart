e8f23c6cc11d15165c672d85df17d5d94a9f7d15
ahmad <ahmadrifka@gmail.com>
2020-03-05 20:28:08 +0700

    [IMPROVE] scrollview

--- lib/app/screen/login.dart
+++ lib/app/screen/login.dart
@@ -9,19 +9,18 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
         child: Container(
           color: Colors.white,
           child: Container(
-            margin: EdgeInsets.all(20.0),
+            margin: EdgeInsets.all(30.0),
+            padding: EdgeInsets.only(top: 15.0),
             child: Column(
               children: <Widget>[
                 Container(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    'Welcome Aboard!',
+                    'Welcome Aboard !',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 30.0,
@@ -32,7 +31,7 @@ class _LoginPageState extends State<LoginPage> {
                   height: 10.0,
                 ),
                 Text(
-                  'Welcome all Talents, enjoy your adventure here in Digital Katalis',
+                  'Welcome to all talents, enjoy your adventure here in Digital Katalis',
                   style: TextStyle(color: Colors.black38, fontSize: 20.0),
                 ),
                 Container(
@@ -61,12 +60,13 @@ Widget form(context) {
         TextField(
           decoration: InputDecoration(
             border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(10.0), borderSide: BorderSide(
-                  color: Colors.purple
-                )),
-            labelText: 'Username',
-            
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(color: Colors.red)
+                ),
+            labelText: 'Username or Email',
+
           ),
+          
         ),
         SizedBox(
           height: 10,
@@ -78,15 +78,19 @@ Widget form(context) {
                   color: Colors.purple
                 )),
             labelText: 'Password',
-            
           ),
+          obscureText: true,
         ),
         Container(
+          margin: EdgeInsets.symmetric(vertical: 10.0),
           width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.07,
           child: RaisedButton(onPressed: (){},
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)) ,
-          color: Colors.pink,
-          child: Text('Login',style: TextStyle(color: Colors.white),),),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)) ,
+            color: Colors.redAccent,
+            child: Text('Login',style: TextStyle(color: Colors.white,
+            fontSize: 20.0
+            ),),),
         )
       ],
     ),
